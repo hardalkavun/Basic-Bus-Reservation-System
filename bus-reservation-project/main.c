4d6e6bf3778d22b67b901683aca4ff2a8260ac89
Eren Kaptan <eren.kaptan@gazi.edu.tr>
2023-12-23 15:01:38 +0300

    Update main.c

--- bus-reservation-project/main.c
+++ bus-reservation-project/main.c
@@ -3,6 +3,34 @@
 
 int main()
 {
-    printf("Hello Eren Kaptan!\n");
+    int choose;
+    printf("+--------------+\n");
+    printf("|| Buy Ticket<1>\n");
+    printf("|| Profile<2>\n");
+    printf("|| My Trips<3>\n");
+    printf("+--------------+\n");
+    printf("Welcome To The ETS Ticket System ! \n");
+
+
+    printf("Please Select An Option:");                             //değiştirilcek
+    scanf("%d",&choose);
+    switch(choose){
+    case '1':
+        printf("---Enter Your Destination---\n");
+        printf("From:");
+        scanf("%c",&from);
+        printf("To:");
+        scanf("%c",&to);
+        // Bilet Al                    // nereden-nereye, kaç bilet,otobüs firması seç,koltuk seç,ödeme,bilet alma başarılı-başarısız mesajı                         //otogarlar,iller,firma,random bilet sayısı,random koltuk sayısı
+        break;
+    case '2':
+            //profil kısmının olduğu                                              //isim soy isim,email,şifre,cinsiyet
+
+        break;
+    case '3':
+          // seyahatlarim kısmı                                                                                  // bilet iptal etme biletin saati falan filan
+        break;
+    }
+
     return 0;
 }
